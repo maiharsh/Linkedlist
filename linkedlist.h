c59bc7f0209a8fac4cbac810cc03b11e8fd814c9
harshvardhan <harshvardhan1813@gmail.com>
2025-12-08 10:41:57 +0530

    Refactor linked list structure and function signatures

--- linkedlist.h
+++ linkedlist.h
@@ -4,20 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NODE1 {
+struct NODE {
     int number;
-    struct NODE1 *next;
+    struct NODE *next;
 };
 
 extern int num1;
-extern struct NODE1 *head;
-extern struct NODE1 *last;
+extern struct NODE *head;
+extern struct NODE *last;
 
-struct NODE1 *create(int value);
+struct NODE *create(int value);
 void find(int value);
 void middleElement(void);
 void delete(void);
 void display(void);
 void insert(int value, int pos);
+void deleteEnd(void);
+void deleteAtPosition(int pos);
+void freeList(void);
+int isEmpty(void);
 
 #endif
