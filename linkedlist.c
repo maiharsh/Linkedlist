fa2e3d7bf9a55dd3e12315a8bb0b3c7496a6546c
harshvardhan <harshvardhan1813@gmail.com>
2025-12-08 10:41:14 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
 int num1 = 0;
+struct NODE *head = NULL;
+struct NODE *last = NULL;
 
-struct NODE1 {
-    int number;
-    struct NODE1 *next;
-};
-
-struct NODE1 *head = NULL;
-struct NODE1 *last = NULL;
-
-struct NODE1 *create(int value){
-    struct NODE1 *new = (struct NODE1 *)malloc(sizeof(struct NODE1));
+struct NODE *create(int value) {
+    struct NODE *new = (struct NODE *)malloc(sizeof(struct NODE));
     new->number = value;
-    if(num1 == 0){
+    
+    if (num1 == 0) {
         head = new;
         last = new;
         new->next = head;
@@ -27,56 +23,73 @@ struct NODE1 *create(int value){
     return new;
 }
 
-void find(int value){
-    struct NODE1 *temp=head;
-    for(int i=0;i<num1;i++){
-        if(temp->number==value){
-            printf("Location At (%p) For Element (%d).\n", temp, temp->number);
+void find(int value) {
+    if (head == NULL) {
+        printf("List empty\n");
+        return;
+    }
+    
+    struct NODE *temp = head;
+    for (int i = 0; i < num1; i++) {
+        if (temp->number == value) {
+            printf("Found %d at pos %d\n", value, i + 1);
             return;
         }
         temp = temp->next;
     }
+    printf("%d not found\n", value);
 }
 
-void middleElement(void){
-    struct NODE1 *t=head;
-    int temp = num1/2;
-    if(num1-temp!=num1) ++temp;
-    for(int i=1;i<temp;i++){
-        t=t->next;
-    }
-    printf("Middle Element Is: %d\n", t->number);
-}
-
-void delete(void){
-    if(num1==0){
-        printf("Error! Linked List Is Empty.\n");
+void middleElement(void) {
+    if (head == NULL) {
+        printf("List empty\n");
         return;
-    } else if(num1==1){
-        struct NODE1 *temp = head;
-        free(temp);
-        head=NULL;
-        last=NULL;
+    }
+    
+    struct NODE *slow = head;
+    struct NODE *fast = head;
+    
+    do {
+        fast = fast->next;
+        if (fast != head) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    } while (fast != head && fast->next != head);
+    
+    printf("Middle: %d\n", slow->number);
+}
+
+void delete(void) {
+    if (num1 == 0) {
+        printf("List empty\n");
+        return;
+    }
+    
+    if (num1 == 1) {
+        free(head);
+        head = NULL;
+        last = NULL;
     } else {
-        struct NODE1 *temp = head;
+        struct NODE *temp = head;
         head = head->next;
-        free(temp);
         last->next = head;
+        free(temp);
     }
     num1--;
 }
 
-void insert(int value, int pos){
-    if(pos < 1 || pos > num1 + 1){
-        printf("Invalid Position!\n");
+void insert(int value, int pos) {
+    if (pos < 1 || pos > num1 + 1) {
+        printf("Invalid pos\n");
         return;
     }
 
-    struct NODE1 *new = (struct NODE1 *)malloc(sizeof(struct NODE1));
+    struct NODE *new = (struct NODE *)malloc(sizeof(struct NODE));
     new->number = value;
 
-    if(pos == 1){
-        if(num1 == 0){
+    if (pos == 1) {
+        if (num1 == 0) {
             head = new;
             last = new;
             new->next = head;
@@ -85,36 +98,107 @@ void insert(int value, int pos){
             last->next = new;
             head = new;
         }
-        num1++;
-        return;
-    }
-
-    if(pos == num1 + 1){
+    } else if (pos == num1 + 1) {
         new->next = head;
         last->next = new;
         last = new;
-        num1++;
-        return;
+    } else {
+        struct NODE *temp = head;
+        for (int i = 1; i < pos - 1; i++) {
+            temp = temp->next;
+        }
+        new->next = temp->next;
+        temp->next = new;
     }
-
-    struct NODE1 *temp = head;
-    for(int i = 1; i < pos - 1; i++){
-        temp = temp->next;
-    }
-    new->next = temp->next;
-    temp->next = new;
     num1++;
 }
 
-void display(void){
-    struct NODE1 *temp=head;
-    for(int i=0;i<num1;i++){
+void display(void) {
+    if (head == NULL) {
+        printf("List empty\n");
+        return;
+    }
+    
+    struct NODE *temp = head;
+    for (int i = 0; i < num1; i++) {
         printf("%d->", temp->number);
         temp = temp->next;
     }
-    if(num1==0){
-        printf("NULL\n");
-    } else {
-        printf("%d(HEAD)\n", head->number);
+    printf("%d(HEAD)\n", head->number);
+}
+
+void deleteEnd(void) {
+    if (num1 == 0) {
+        printf("List empty\n");
+        return;
     }
+    
+    if (num1 == 1) {
+        free(head);
+        head = NULL;
+        last = NULL;
+    } else {
+        struct NODE *temp = head;
+        struct NODE *prev = NULL;
+        
+        while (temp->next != head) {
+            prev = temp;
+            temp = temp->next;
+        }
+        
+        prev->next = head;
+        last = prev;
+        free(temp);
+    }
+    num1--;
+}
+
+void deleteAtPosition(int pos) {
+    if (pos < 1 || pos > num1) {
+        printf("Invalid pos\n");
+        return;
+    }
+    
+    if (pos == 1) {
+        delete();
+        return;
+    }
+    
+    if (pos == num1) {
+        deleteEnd();
+        return;
+    }
+    
+    struct NODE *temp = head;
+    struct NODE *prev = NULL;
+    
+    for (int i = 1; i < pos; i++) {
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    prev->next = temp->next;
+    free(temp);
+    num1--;
+}
+
+void freeList(void) {
+    if (head == NULL) return;
+    
+    struct NODE *temp = head;
+    struct NODE *nextNode;
+    
+    for (int i = 0; i < num1; i++) {
+        nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+    
+    head = NULL;
+    last = NULL;
+    num1 = 0;
+}
+
+int isEmpty(void) {
+    return head == NULL;
 }
